79c80c63a9e3b62a1ec917a8ab45a57b9f43ff0f
sccherry <sccherry@users.noreply.github.com>
2020-12-09 19:41:23 +0000

    deploy: 997591c9191c433fa4c8107b3a05e03b0e3c8763

---
+++ code/kick-drum/script.js
@@ -0,0 +1,58 @@
+
+import { createAmplitudeEnvelope, createOmniOscillator, createPiano } from '/code/2017/02/07/nexus-tone-components/script.js';
+
+const options = {
+  synth: {
+    pitchDecay: 0.005,
+    envelope: {
+      decay: 0.04,
+      sustain: 0,
+      release: 0.04
+    }
+  }
+};
+
+const synth = new Tone.MembraneSynth(options.synth).toMaster();
+
+const onMidi = ([status, data0, data1]) => {
+  switch (status) {
+    case 144: {
+      const freq = Tone.Midi(data0).toFrequency();
+      synth.triggerAttack(freq, '+0', data1 / 127);
+      break;
+    }
+    case 128: {
+      synth.triggerRelease();
+      break;
+    }
+  }
+};
+
+const { oscillator, envelope, pitchDecay, octaves } = synth.get();
+
+createOmniOscillator('#synth-oscillator', oscillator, (value) => {
+  synth.set({ oscillator: value });
+});
+
+createAmplitudeEnvelope('#synth-envelope', envelope, (value) => {
+  synth.set({ envelope: value });
+});
+
+new Nexus.Slider('#synth-pitch-decay', {
+  step: 0.001,
+  value: pitchDecay
+}).on('change', (value) => {
+  synth.set('pitchDecay', value);
+});
+
+new Nexus.Slider('#synth-octaves', {
+  min: 1,
+  max: 20,
+  step: 1,
+  value: octaves
+}).on('change', (value) => {
+  synth.set('octaves', value);
+});
+
+createPiano('#piano', { mode: 'button' }, onMidi);
+
